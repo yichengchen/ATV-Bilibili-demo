da22ad153d50979cae97ecd603324b5b260a1b77
yicheng <11733500+yichengchen@users.noreply.github.com>
2022-10-19 21:35:37 +0800

    fix: long press back button no working

--- BilibiliLive/BLTabBarViewController.swift
+++ BilibiliLive/BLTabBarViewController.swift
@@ -63,6 +63,7 @@ class BLTabBarViewController: UITabBarController, UITabBarControllerDelegate {
     }
     
     override func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        super.pressesEnded(presses, with: event)
         guard let buttonPress = presses.first?.type else { return }
         if buttonPress == .playPause {
             if let reloadVC = topMostViewController() as? BLTabBarContentVCProtocol {
