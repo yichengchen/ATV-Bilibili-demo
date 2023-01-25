18af7542b24259719ad107f4c2c242dac50567de
hydewww <28640884+hydewww@users.noreply.github.com>
2023-01-25 18:38:18 +0800

    fix: dlna ip

--- BilibiliLive/Module/DLNA/BiliBiliUpnpDMR.swift
+++ BilibiliLive/Module/DLNA/BiliBiliUpnpDMR.swift
@@ -171,10 +171,13 @@ class BiliBiliUpnpDMR: NSObject {
                 let addrFamily = interface.ifa_addr.pointee.sa_family
                 if addrFamily == UInt8(AF_INET) {
                     let name = String(cString: interface.ifa_name)
-                    if name == "en0" || name == "en2" || name == "en3" || name == "en4" {
+                    if name == "en0" || name == "en1" || name == "en2" || name == "en3" || name == "en4" {
                         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
                         getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
                         address = String(cString: hostname)
+                        if name == "en0" {
+                            break
+                        }
                     }
                 }
             }
